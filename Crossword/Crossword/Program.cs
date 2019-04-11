bb79f441f0a03bd92e1312463a61c986092c5f8e
Alexandra Lamiova Pistrakova <alex.pistrakova@gmail.com>
2019-04-11 21:37:13 +0200

    Crossword

--- Crossword/Crossword/Program.cs
+++ Crossword/Crossword/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Crossword
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> names = new List<string>() { "mery","alex", "jozko", "ale" };
+
+            Console.WriteLine("Before sort");
+            names.ForEach(Console.WriteLine);
+            Console.WriteLine("=============================");
+
+            Sort(names);
+
+            Console.WriteLine("After sort:");
+            names.ForEach(Console.WriteLine);
+
+            Console.ReadKey();
+        }
+
+        private static void Sort(List<string> names)
+        {
+            names.Sort(CompareNames);
+        }
+
+        private static int CompareNames(string name1, string name2)
+        {
+            int l1 = name1.Length;
+            int l2 = name2.Length;
+
+            if (l1 == l2)
+                return string.Compare(name1, name2, true);
+
+            return l1 - l2;
         }
     }
 }
