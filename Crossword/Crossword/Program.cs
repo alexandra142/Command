48246302da19d78574aa938b50eb684dcfd8fcd2
Alexandra Lamiova Pistrakova <alex.pistrakova@gmail.com>
2019-04-11 21:17:33 +0200

    Project Setup

---
+++ Crossword/Crossword/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crossword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
